ea4b0c40ed5ffae29a6fee5bb92d2d887c0d160e
MauiGonzo <maurice.verbrugge@gmail.com>
2020-03-03 11:49:53 +0100

    added arg_parse. see readme for basic option string

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MLiP-2020
 Repository for the Machine Learning in Practice course at the Radboud University in Nijmegen
+
+init (and use defaults) by using *python3 main.py -i='kaggle/input/train_image_data_64.npy' -l='kaggle/input/train.csv' -drop='gridmask' -is=64 -m='model.pt'*
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import torch
+import argparse
 from torch.optim import Adam
 from torchsummary import summary  # pip install torchsummary
 
@@ -9,21 +10,64 @@ from optim import train
 from utils.data import load_data
 from utils.tensorboard import MetricsWriter
 
-IMAGES = r'kaggle\input\bengaliai-cv19\train_image_data_64.npy'
-LABELS = r'kaggle\input\bengaliai-cv19\train.csv'
-TEST_RATIO = 0.2
-NUM_EPOCHS = 50
-DATA_AUGMENTATION = True
-DROP_INFO_FUNCTION = 'gridmask'  # 'cutout' or None
-CLASS_BALANCING = True
-BATCH_SIZE = 32
+# IMAGES = r'kaggle\input\bengaliai-cv19\train_image_data_64.npy'
+# LABELS = r'kaggle\input\bengaliai-cv19\train.csv'
+# TEST_RATIO = 0.2
+# NUM_EPOCHS = 50
+# DATA_AUGMENTATION = True
+# DROP_INFO_FUNCTION = 'gridmask'  # 'cutout' or None
+# CLASS_BALANCING = True
+# BATCH_SIZE = 32
 IMAGE_SIZE = 64
 MODEL = 'model.pt'
 
 if __name__ == '__main__':
+    """
+    handles input arguments
+    main --help gives overview
+    """
+    drop_info_func = ['cutout', 'gridmask', 'None']
+    # process the command options
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--images', '-i', type=str,
+                        help=r'provide path in style: kaggle\input\bengaliai-cv19\train_image_data_64.npy')
+    parser.add_argument('--labels', '-l', type=str,
+                        help=r'provide path in style: kaggle\input\bengaliai-cv19\train.csv')
+    parser.add_argument('--test_ratio', '-tr', type=float, default=0.2,
+                        help='[float] percentage of data used for validation, default tr=0.2')
+    parser.add_argument('--num_epochs', '-e', type=int, default=50,
+                        help='[int] number of iterations over the train data set, default e=50')
+    parser.add_argument('--data_augmentation', '-da', type=bool, default=False,
+                        help='[bool] whether or not the images are transformed, default da=False')
+    parser.add_argument('--drop_info_function', '-drop', type=str, choices=drop_info_func,
+                        help=r'[str] whether to use cutout, gridmask or no info dropping algorithm i.e. None')
+    parser.add_argument('--class_balancing', '-cb', type=bool, default=False,
+                        help='[bool] whether or not the classes are balanced')
+    parser.add_argument('--batch_size', '-bs', type=int, default=32,
+                        help='[int] batch size of the DataLoader objects default bs=32')
+    parser.add_argument('--image_size', '-is', type=int,
+                        help='[int] image pixel size of the input image, assumed square image')
+    parser.add_argument('--model', '-m', type=str,
+                        help='[str]  model name such as model.pt')
+    args = parser.parse_args()
     # load training and validation data
-    data = load_data(IMAGES, LABELS, TEST_RATIO, NUM_EPOCHS, DATA_AUGMENTATION,
-                     DROP_INFO_FUNCTION, CLASS_BALANCING, BATCH_SIZE)
+
+    # print('IMAGES = {}'.format(args.images))
+    # print('LABELS = {}'.format(args.labels))
+    # print('TEST_RATIO = {}'.format(args.test_ratio))
+    # print('NUM_EPOCHS = {}'.format(args.num_epochs))
+    # print('DATA_AUGMENTATION = {}'.format(args.data_augmentation))
+    # print('DROP INFO FUNC = {}'.format(args.drop_info_function))
+    # print('CLASS BALANCING = {}'.format(args.class_balancing))
+    # print('BATCH SIZE = {}'.format(args.batch_size))
+    data = load_data(args.images,
+                     args.labels,
+                     args.test_ratio,
+                     args.num_epochs,
+                     args.data_augmentation,
+                     args.drop_info_function,
+                     args.class_balancing,
+                     args.batch_size)
     train_dataset, train_loader, val_loader = data
 
     # use GPU if available
@@ -32,7 +76,7 @@ if __name__ == '__main__':
 
     # initialize network and show summary
     model = ZeroNet(device).train()
-    summary(model, input_size=(1, IMAGE_SIZE, IMAGE_SIZE), device=str(device))
+    summary(model, input_size=(1, args.image_size, args.image_size), device=str(device))
 
     # initialize optimizer and criterion
     optimizer = Adam(model.parameters(), lr=0.001)
@@ -49,4 +93,4 @@ if __name__ == '__main__':
           val_loader, val_writer, optimizer, criterion)
 
     # save model to storage
-    torch.save(model.state_dict(), MODEL)
+    torch.save(model.state_dict(), args.model)
