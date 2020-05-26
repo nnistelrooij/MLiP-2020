4931024969fde0519156b0f68254f520bb880335
rusane <43827639+rusane@users.noreply.github.com>
2020-05-26 21:31:07 +0200

    Continue with own LightGBM adaptation

--- LightGBM/data.py
+++ LightGBM/data.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 def downcast(df, verbose=False):
     """
-    Downcasts the data to reduce memory usage.
-    Adapted from: https://www.kaggle.com/kyakovlev/m5-simple-fe
+    Downcast the data to reduce memory usage.
+    Adapted from: https://www.kaggle.com/ragnar123/very-fst-model
 
     Args:
         df = [pd.DataFrame] pandas dataframe
@@ -36,13 +36,7 @@ def downcast(df, verbose=False):
                 elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                     df[col] = df[col].astype(np.float32)
                 else:
-                    df[col] = df[col].astype(np.float64)   
-        elif col == 'date':
-            df[col] = pd.to_datetime(df[col], format='%Y-%m-%d')
-        elif str(col_type) == 'object':
-            df[col] = df[col].astype('category')
-        else:
-            print(f'Unexpected dtype {col_type} in dataframe.')   
+                    df[col] = df[col].astype(np.float64)
 
     end_mem = df.memory_usage().sum() / 1024**2
     if verbose: 
@@ -50,22 +44,85 @@ def downcast(df, verbose=False):
     return df
 
 
-def cat_to_int(df, ignore=[]):
+def obj_as_cat_int(df, ignore=[]):
     """
-    Convert categorical columns to integer.
+    Convert object columns to categorical integers.
 
     Args:
         df = [pd.DataFrame] pandas dataframe
     
     Returns [pd.DataFrame]:
-        Data where categorical columns are encoded as integers.
+        Data where object columns are encoded as categorical integers.
     """
-    cat_cols = df.select_dtypes(include='category').columns
-    for col in cat_cols:
+    obj_cols = df.select_dtypes(include='object').columns
+    for col in obj_cols:
         if col not in ignore:
+            df[col] = df[col].astype('category')
             df[col] = df[col].cat.codes.astype("int16")
             df[col] -= df[col].min()
     return df
+
+
+def optimize_df(calendar, prices, sales, days=None, verbose=False):
+    """
+    Optimize dataframe.
+
+    Args:
+        calendar = [pd.DataFrame] dates of product sales
+        prices   = [pd.DataFrame] price of the products sold per store and date
+        sales    = [pd.DataFrame] historical daily unit sales data per product and store 
+        days     = [int] number of days to keep
+        verbose  = [boolean] if True, print memory reduction
+
+    Returns [(pd.DataFrame) * 3]
+        Optimized dataframes.s
+    """
+    assert days > 56, "Minimum days is 57."
+    assert days < 1914, "Maximum days is 1913."
+    calendar['date'] = pd.to_datetime(calendar['date'], format='%Y-%m-%d')
+
+    if days:
+        sales.drop(sales.columns[6:-days], axis=1, inplace=True)
+
+    calendar = downcast( obj_as_cat_int(calendar, ignore=['d']), verbose )
+    prices = downcast( obj_as_cat_int(prices), verbose )
+    sales = downcast( obj_as_cat_int(sales, ignore=['id']), verbose )
+
+    return calendar, prices, sales
+
+
+def features(df):
+    """
+    Create features.
+    Adapted from: https://www.kaggle.com/kneroma/m5-first-public-notebook-under-0-50
+
+    Args:
+        df = [pd.DataFrame] wide format dataframe
+    """
+    lags = [7, 28]
+    lag_cols = [f"lag_{lag}" for lag in lags]
+    for lag, lag_col in zip(lags, lag_cols):
+        df[lag_col] = df[["id", "sales"]].groupby("id")["sales"].shift(lag)
+
+    windows = [7, 28]
+    for window in windows :
+        for lag,lag_col in zip(lags, lag_cols):
+            df[f"rmean_{lag}_{window}"] = df[["id", lag_col]].groupby("id")[lag_col].transform(lambda x: x.rolling(window).mean())
+
+    date_features = {
+                     "wday": "weekday",
+                     "week": "weekofyear",
+                     "month": "month",
+                     "quarter": "quarter",
+                     "year": "year",
+                     "mday": "day"
+                    }
+    
+    for name, attribute in date_features.items():
+        if name in df.columns:
+            df[name] = df[name].astype("int16")
+        else:
+            df[name] = getattr(df["date"].dt, attribute).astype("int16")
 
 
 if __name__ == "__main__":
